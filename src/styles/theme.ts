d843557f430ec1b0e5f015cc865759f7f6a28e26
Evan K <ekerik220@gmail.com>
2020-09-12 17:08:04 +0900

    Set up theme and global styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  fontFamily:
+    'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif',
+  colors: {
+    mainBackground: "#f6f9fc",
+    primaryColor: "#747fe0",
+    secondaryColor: "white",
+    primaryFontColor: "#525f7f",
+    skillBorderColor: "#92eac0",
+  },
+};
